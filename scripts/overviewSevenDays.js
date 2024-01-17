7085a7558a4ccbef42fb6a497ea97206ccca9500
hhack <hans.f.hack@googlemail.com>
2024-01-17 11:58:50 +0100

    replaced search dataset link

--- scripts/overviewSevenDays.js
+++ scripts/overviewSevenDays.js
@@ -64,9 +64,12 @@ async function filterSavetoCSV(data) {
       d.activityType === "Neu"
         ? convertTimestamp(d.date_released)
         : convertTimestamp(d.date_updated);
+    // const link =
+    //   "https://daten.berlin.de/datensaetze/"
     const link =
-      "https://daten.berlin.de/datensaetze/" +
-      title.replaceAll(",", "").replaceAll(" - ", "-").replaceAll(" ", "-");
+      "https://daten.berlin.de/search/node/" +
+      title.toString().replace(/\s/g, "%20");
+    title.replaceAll(",", "").replaceAll(" - ", "-").replaceAll(" ", "-");
     oneEntry.Titel = `<a style='text-decoration: underline; line-height: 1.4;color: #222' href='${link}' target='_blank'>${title}</a>`;
     oneEntry.Art = d.activityType;
     filteredData.push(oneEntry);
