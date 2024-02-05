c26bde8b20bbfc44ab01e1ff7aeb6430602f8840
hhack <hans.f.hack@googlemail.com>
2024-02-05 14:26:25 +0100

    update years script

--- scripts/overviewYears.js
+++ scripts/overviewYears.js
@@ -3,6 +3,44 @@ const fs = require("fs");
 const { format } = require("fast-csv");
 // const { rawAll } = require("../data/rawAll");
 
+const baseUrl = "https://datenregister.berlin.de/api/3/action/package_search";
+const rowsPerPage = 1000;
+let start = 0;
+let allEntries = [];
+
+const fetchAllEntries = async () => {
+  while (true) {
+    const url = `${baseUrl}?start=${start}&rows=${rowsPerPage}`;
+
+    console.log("fetching", start, " ");
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.success) {
+        const packages = data.result.results;
+        if (packages.length === 0) {
+          // No more results, break the loop
+          break;
+        }
+
+        // Add the current page of results to the allEntries array
+        allEntries = allEntries.concat(packages);
+        start += rowsPerPage;
+      } else {
+        console.error("API request failed:", data.error);
+        break;
+      }
+    } catch (error) {
+      console.error("Error making API request:", error);
+      break;
+    }
+  }
+
+  return allEntries;
+};
+
 const dates = {};
 
 const getJSON = async (url) => {
@@ -27,73 +65,70 @@ function sortByDate(obj) {
 }
 
 async function sumFileFormats() {
-  //   const allData = getJSON("./data/rawAll.json");
-  //   const rawAll = JSON.parse(fs.readFileSync("./data/rawAll.json"));
+  fetchAllEntries()
+    .then((data) => {
+      data.forEach((res) => {
+        const yearMonthUpdate = res.date_updated?.slice(0, 4);
+        const yearMonthRelease = res.date_released?.slice(0, 4);
 
-  const data = await getJSON(
-    "https://datenregister.berlin.de/api/3/action/package_search?start=0&rows=1000000"
-  );
+        const currentDate = new Date();
+        const lastYear = currentDate.getFullYear() - 1;
 
-  data.result.results.forEach((res) => {
-    const yearMonthUpdate = res.date_updated?.slice(0, 4);
-    const yearMonthRelease = res.date_released?.slice(0, 4);
+        if (
+          yearMonthUpdate < 2011 ||
+          yearMonthRelease < 2011 ||
+          yearMonthUpdate > lastYear ||
+          yearMonthRelease > lastYear
+        ) {
+          return;
+        }
 
-    const currentDate = new Date();
-    const lastYear = currentDate.getFullYear() - 1;
+        if (
+          // release
+          res.date_updated === res.date_released ||
+          res.date_updated === undefined
+        ) {
+          if (!dates[yearMonthRelease]) {
+            dates[yearMonthRelease] = { update: 0, release: 0 };
+          }
+          dates[yearMonthRelease].release = dates[yearMonthRelease].release + 1;
+        } else {
+          // update
+          if (!dates[yearMonthUpdate]) {
+            dates[yearMonthUpdate] = { update: 0, release: 0 };
+          }
+          dates[yearMonthUpdate].update = dates[yearMonthUpdate].update + 1;
+        }
+      });
 
-    if (
-      yearMonthUpdate < 2011 ||
-      yearMonthRelease < 2011 ||
-      yearMonthUpdate > lastYear ||
-      yearMonthRelease > lastYear
-    ) {
-      return;
-    }
+      const sortedData = sortByDate(dates);
 
-    if (
-      // release
-      res.date_updated === res.date_released ||
-      res.date_updated === undefined
-    ) {
-      if (!dates[yearMonthRelease]) {
-        dates[yearMonthRelease] = { update: 0, release: 0 };
+      const header = ["date"];
+      const update = ["Updates"];
+      const release = ["Neuerscheinungen"];
+
+      for (const key in sortedData) {
+        header.push(key);
+        update.push(sortedData[key].update);
+        release.push(sortedData[key].release);
       }
-      dates[yearMonthRelease].release = dates[yearMonthRelease].release + 1;
-    } else {
-      // update
-      if (!dates[yearMonthUpdate]) {
-        dates[yearMonthUpdate] = { update: 0, release: 0 };
-      }
-      dates[yearMonthUpdate].update = dates[yearMonthUpdate].update + 1;
-    }
-  });
 
-  const sortedData = sortByDate(dates);
+      let csv = header.toString() + "\n";
+      csv += update.toString() + "\n";
+      csv += release.toString() + "\n";
 
-  const header = ["date"];
-  const update = ["Updates"];
-  const release = ["Neuerscheinungen"];
-
-  for (const key in sortedData) {
-    header.push(key);
-    update.push(sortedData[key].update);
-    release.push(sortedData[key].release);
-  }
-
-  let csv = header.toString() + "\n";
-  csv += update.toString() + "\n";
-  csv += release.toString() + "\n";
-
-  console.log(csv);
-
-  fs.writeFile(
-    "./data/overviewYears.csv",
-    csv,
-    {
-      encoding: "utf8",
-    },
-    (err) => {}
-  );
+      fs.writeFile(
+        "./data/overviewYears.csv",
+        csv,
+        {
+          encoding: "utf8",
+        },
+        (err) => {}
+      );
+    })
+    .catch((error) => {
+      console.error("Error fetching entries:", error);
+    });
 }
 
 sumFileFormats();
